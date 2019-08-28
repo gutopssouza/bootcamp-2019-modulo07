42a1d475541ef9dcdc4770201613f0399381e5df
Augusto <gutopssouza@gmail.com>
2019-08-28 15:34:00 -0300

    :art: Create Home ListItens styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,100 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1 />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/camisa-avai-i-1920-sn-torcedor-umbro-masculina/43/D21-3395-543/D21-3395-543_zoom1.jpg?ims=544x"
+          alt="Camisa Avaí"
+        />
+        <strong>Camisa do Avaí FC</strong>
+        <span>R$100,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/camisa-avai-i-1920-sn-torcedor-umbro-masculina/43/D21-3395-543/D21-3395-543_zoom1.jpg?ims=544x"
+          alt="Camisa Avaí"
+        />
+        <strong>Camisa do Avaí FC</strong>
+        <span>R$100,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/camisa-avai-i-1920-sn-torcedor-umbro-masculina/43/D21-3395-543/D21-3395-543_zoom1.jpg?ims=544x"
+          alt="Camisa Avaí"
+        />
+        <strong>Camisa do Avaí FC</strong>
+        <span>R$100,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/camisa-avai-i-1920-sn-torcedor-umbro-masculina/43/D21-3395-543/D21-3395-543_zoom1.jpg?ims=544x"
+          alt="Camisa Avaí"
+        />
+        <strong>Camisa do Avaí FC</strong>
+        <span>R$100,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/camisa-avai-i-1920-sn-torcedor-umbro-masculina/43/D21-3395-543/D21-3395-543_zoom1.jpg?ims=544x"
+          alt="Camisa Avaí"
+        />
+        <strong>Camisa do Avaí FC</strong>
+        <span>R$100,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/camisa-avai-i-1920-sn-torcedor-umbro-masculina/43/D21-3395-543/D21-3395-543_zoom1.jpg?ims=544x"
+          alt="Camisa Avaí"
+        />
+        <strong>Camisa do Avaí FC</strong>
+        <span>R$100,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
