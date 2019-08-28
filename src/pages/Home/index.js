ee127fc01484f2aeb3f7a190cd275434267c5a2e
Augusto <gutopssouza@gmail.com>
2019-08-28 18:29:01 -0300

    :rocket: Implement home list itens to get data from the api :rocket: Create format.js to format price to BRL currency

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,100 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
-
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/camisa-avai-i-1920-sn-torcedor-umbro-masculina/43/D21-3395-543/D21-3395-543_zoom1.jpg?ims=544x"
-          alt="Camisa Avaí"
-        />
-        <strong>Camisa do Avaí FC</strong>
-        <span>R$100,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/camisa-avai-i-1920-sn-torcedor-umbro-masculina/43/D21-3395-543/D21-3395-543_zoom1.jpg?ims=544x"
-          alt="Camisa Avaí"
-        />
-        <strong>Camisa do Avaí FC</strong>
-        <span>R$100,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+  async componentDidMount() {
+    const response = await api.get('products');
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+    this.setState({ products: data });
+  }
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/camisa-avai-i-1920-sn-torcedor-umbro-masculina/43/D21-3395-543/D21-3395-543_zoom1.jpg?ims=544x"
-          alt="Camisa Avaí"
-        />
-        <strong>Camisa do Avaí FC</strong>
-        <span>R$100,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/camisa-avai-i-1920-sn-torcedor-umbro-masculina/43/D21-3395-543/D21-3395-543_zoom1.jpg?ims=544x"
-          alt="Camisa Avaí"
-        />
-        <strong>Camisa do Avaí FC</strong>
-        <span>R$100,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/camisa-avai-i-1920-sn-torcedor-umbro-masculina/43/D21-3395-543/D21-3395-543_zoom1.jpg?ims=544x"
-          alt="Camisa Avaí"
-        />
-        <strong>Camisa do Avaí FC</strong>
-        <span>R$100,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/camisa-avai-i-1920-sn-torcedor-umbro-masculina/43/D21-3395-543/D21-3395-543_zoom1.jpg?ims=544x"
-          alt="Camisa Avaí"
-        />
-        <strong>Camisa do Avaí FC</strong>
-        <span>R$100,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
