2c03a09d004440d573f65cb937c6607532ba3cd1
Augusto <gutopssouza@gmail.com>
2019-08-28 21:16:47 -0300

    :art: Refactor actions remove from cart and add to cart

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 import { ProductList } from './styles';
+import * as CartActions from '../../store/modules/cart/actions';
 
 class Home extends Component {
   state = {
@@ -20,12 +22,9 @@ class Home extends Component {
   }
 
   handleAddProduct = product => {
-    const { dispatch } = this.props;
+    const { addToCart } = this.props;
 
-    dispatch({
-      type: 'ADD_TO_CART',
-      product,
-    });
+    addToCart(product);
   };
 
   render() {
@@ -53,4 +52,10 @@ class Home extends Component {
   }
 }
 
-export default connect()(Home);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Home);
